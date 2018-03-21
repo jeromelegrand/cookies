4a61d2d5a29024c3673773ed47b64a464c5fd77c
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-03-21 13:18:57 +0100

    nettoyage du code

--- addToCart.php
+++ addToCart.php
@@ -1,12 +1,10 @@
 <?php
 
-var_dump($_GET);
-var_dump($_COOKIE);
+
 
 //modification des cookie
 if (isset($_GET['add_to_cart'])) {
     $ref = $_GET['add_to_cart'];
-    var_dump($ref);
     if (isset($_COOKIE[$ref])) {
         $amount = $_COOKIE[$ref] + 1;
         setcookie($ref, $amount, time() + 60 * 10);
